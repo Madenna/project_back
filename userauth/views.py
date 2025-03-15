90e664d64c68496fab832840ddad012855c2eb63
Madina Muratkyzy <madenna@Madinas-MacBook-Air.local>
2025-03-15 16:38:57 +0500

    importing serializer

--- userauth/views.py
+++ userauth/views.py
@@ -596,6 +596,7 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.views import APIView
 from drf_yasg.utils import swagger_auto_schema
+from rest_framework import serializers  
 
 from .serializers import (
     UserSerializer, LoginSerializer, RegisterSerializer, OTPVerificationSerializer,
