3a36e8eb13679f3ee90da680633891f9c57e679d
Madina Muratkyzy <madenna@Madinas-MacBook-Air.local>
2025-04-08 21:16:07 +0500

    found errors

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from .views import (
     EquipmentItemListCreateView,
     EquipmentItemDetailView,
-    EquipmentItemListView,
-    EquipmentCategoryListView,  # ✅ new view
+    PublicEquipmentListView,
+    EquipmentCategoryListView,  
 )
 
 urlpatterns = [
     path("my-items/", EquipmentItemListCreateView.as_view(), name="my_equipment_list_create"),
     path("my-items/<uuid:id>/", EquipmentItemDetailView.as_view(), name="my_equipment_detail"),
-    path("public-items/", EquipmentItemListView.as_view(), name="public_equipment_list"),
+    path("public-items/", PublicEquipmentListView.as_view(), name="public_equipment_list"),
     path("categories/", EquipmentCategoryListView.as_view(), name="equipment_category_list"),  
 ]
